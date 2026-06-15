4ec2b7cfc0667c2f39faa302bbcf69187298da87
Henri Golo <henri.golo.313f@gmail.com>
2026-06-15 14:59:04 +0100

    ajout d'une commande pour changer le nom / avatar / bannière du bot

--- cogs/__init__.py
+++ cogs/__init__.py
@@ -1,3 +1,6 @@
+import io
+
+import aiohttp
 import discord
 from discord import ApplicationContext as AppCtx
 from discord.ext import commands
@@ -17,3 +20,31 @@ class HelloWorld(MarinovCog):
     async def ping(self, ctx: AppCtx):
         await ctx.defer(ephemeral=True)
         await ctx.respond("pong")
+
+
+class Customisation(MarinovCog):
+    @commands.slash_command()
+    @logger
+    async def avatar(self, ctx: AppCtx, nom: str = '', avatar_url: str = '', banner_url: str = ''):
+        await ctx.defer(ephemeral=True)
+        # Nom, PP et Bannière
+        kwargs = dict()
+        if nom:
+            kwargs['username'] = nom
+        if avatar_url or banner_url:
+            async with aiohttp.ClientSession() as session:
+                if avatar_url:
+                    async with session.get(marinovka.icon.url) as resp:
+                        img = await resp.read()
+                        with io.BytesIO(img) as file:
+                            kwargs['avatar'] = file.getvalue()
+                if banner_url:
+                    async with session.get(banner_url) as resp:
+                        img = await resp.read()
+                        with io.BytesIO(img) as file:
+                            kwargs['banner'] = file.getvalue()
+        if kwargs:
+            await self.bot.user.edit(**kwargs)
+            await ctx.respond("Bot modifié")
+        else:
+            await ctx.respond("Rien à faire")
