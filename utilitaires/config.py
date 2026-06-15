9c591652396a503d247e0810d0be57fcdd2e66e4
Henri Golo <henri.golo.313f@gmail.com>
2026-06-15 23:15:45 +0100

    ajout d'un champ pour définir le formattage json

--- utilitaires/config.py
+++ utilitaires/config.py
@@ -16,6 +16,7 @@ class Config:
             **os.environ,
         }
         self.channel_logs: discord.Thread = None
+        self.json_format = {}
 
     def __getitem__(self, item):
         return self.values[item]
