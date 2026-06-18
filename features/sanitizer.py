30005c2fe6a7434792b9164734614524ff792e8c
Henri Golo <henri.golo.313f@gmail.com>
2026-06-18 14:42:33 +0100

    amélioration du sanitizer, meilleur stockage, meilleure lecture

--- features/sanitizer.py
+++ features/sanitizer.py
@@ -7,6 +7,10 @@ from utilitaires import Embed, ButtonModal, fail
 from utilitaires.config import config
 
 
+def short_netloc(netloc: str):
+    return netloc.replace('www.', '')
+
+
 class AddException(discord.ui.DesignerModal):
     def __init__(self, urls, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -14,7 +18,7 @@ class AddException(discord.ui.DesignerModal):
             discord.ui.Label(
                 'Domaine',
                 item=discord.ui.Select(
-                    options=[discord.SelectOption(label=url.netloc) for url in urls],
+                    options=[discord.SelectOption(label=short_netloc(url.netloc)) for url in urls],
                     placeholder='Choisir un domaine',
                     required=True)
             )
@@ -23,7 +27,8 @@ class AddException(discord.ui.DesignerModal):
             discord.ui.Label(
                 'Paramètre Autorisé',
                 item=discord.ui.Select(
-                    options=[discord.SelectOption(label=q[0], description=q[1]) for url in urls for q in parse_qsl(url.query)],
+                    options=[discord.SelectOption(label=q[0], description=q[1]) for url in urls for q in
+                             parse_qsl(url.query)],
                     placeholder='Ajouter une query',
                     required=True,
                     max_values=len(sum((parse_qsl(url.query) for url in urls), []))
@@ -33,14 +38,16 @@ class AddException(discord.ui.DesignerModal):
 
     async def callback(self, interaction: discord.Interaction):
         netloc = self.children[0].item.values[0]
+        short = short_netloc(netloc)
         with open(Sanitizer.exceptions, 'r') as file:
             exceptions = json.load(file, **config.json_format)
-        exceptions[netloc] = list(set(exceptions.get(netloc, []) + self.children[1].item.values))
+        exceptions[short] = list(set(exceptions.get(short, []) + self.children[1].item.values))
         with open(Sanitizer.exceptions, 'w') as file:
             json.dump(exceptions, file, **config.json_format)
         await interaction.respond(
-            f"Liste des exceptions pour {netloc} : {', '.join(exceptions[netloc])}",
-            ephemeral=True
+            f"Liste des exceptions pour {short} : {', '.join(exceptions[short])}",
+            ephemeral=True,
+            delete_after=3,
         )
         # Rerun l'analyse des liens
         try:
@@ -77,9 +84,9 @@ class Sanitizer:
                 urls.append(url)
         return urls
 
-    def _sanitize(self, url: SplitResult) -> SplitResult:
-        exceptions: dict[str, list[str]] = json.load(open(self.exceptions, 'r'), **config.json_format)
-        allowed = exceptions.get(url.netloc, [])
+    @staticmethod
+    def _sanitize(url: SplitResult, exceptions: dict[str, list[str]]) -> SplitResult:
+        allowed = exceptions.get(short_netloc(url.netloc), [])
         queries = {k: v for k, v in parse_qs(url.query).items() if k in allowed}
         return url._replace(query=urlencode(queries, doseq=True))
 
@@ -92,7 +99,9 @@ class Sanitizer:
                     description="En fonction du nom de domaine, potentiellement trop fort\n"
                 )
                 view = discord.ui.View()
-                surls = list(map(self._sanitize, urls))
+                with open(self.exceptions, 'r') as file:
+                    exceptions = json.load(file, **config.json_format)
+                surls = list(map(lambda u: self._sanitize(u, exceptions), urls))
                 for i, surl in enumerate(surls):
                     if urlunsplit(surl) == urlunsplit(urls[i]):
                         continue
